25ee0eeb8340b0b97f98393a1651ea9c324e6251
Aayush Chaudhary <acaayush1111@gmail.com>
2025-02-01 12:17:24 +0530

    model loading & basic setup

---
+++ backend/model.py
@@ -0,0 +1,19 @@
+from transformers import DistilBertModel, DistilBertTokenizer
+
+def load_model(): 
+    tokenizer = DistilBertTokenizer.from_pretrained(
+        "distilbert-base-uncased", 
+    )
+
+    model = DistilBertModel.from_pretrained(
+        "distilbert-base-uncased",
+        output_attentions=True
+    )
+
+    model.eval()
+
+    return tokenizer, model
+
+if __name__ == "__main__":
+    tokenizer, model = load_model()
+    print("Model and tokenizer loaded successfully.")
